01605a2d3c6d4003a4dbdb3481d981b095642235
TamannaSheme <syedatsheme03@gmail.com>
2025-04-03 07:18:02 +1100

    role page done

---
+++ js/role.js
@@ -0,0 +1,6 @@
+function handleRole(role) {
+    alert("You selected: " + role);
+    // You can redirect to role-specific dashboards here
+    // window.location.href = `${role}-dashboard.html`;
+  }
+  
