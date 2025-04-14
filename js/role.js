3df54b32ed05495f0b847a2f0be01dc8f4fbb13d
TamannaSheme <syedatsheme03@gmail.com>
2025-04-15 09:53:31 +1000

    student page added

--- js/role.js
+++ js/role.js
@@ -3,4 +3,13 @@ function handleRole(role) {
     // You can redirect to role-specific dashboards here
     // window.location.href = `${role}-dashboard.html`;
   }
+  function handleRole(role) {
+    if (role === 'student') {
+      window.location.href = "student.html";
+    } else if (role === 'instructor') {
+      window.location.href = "instructor.html";
+    } else if (role === 'admin') {
+      window.location.href = "admin.html";
+    }
+  }
   
