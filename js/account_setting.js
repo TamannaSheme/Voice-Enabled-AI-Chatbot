e9dcb73e686ceba4a94245dad713c77f4c6798fd
TamannaSheme <syedatsheme03@gmail.com>
2025-04-28 19:49:31 +1000

    Account Page Updated

--- js/account_setting.js
+++ js/account_setting.js
@@ -1,11 +1,9 @@
-// account_setting.js
-
 document.getElementById("save-btn").addEventListener("click", function() {
     alert("Account settings saved successfully!");
   });
   
   document.getElementById("logout-btn").addEventListener("click", function() {
     alert("You have been logged out!");
-    window.location.href = "login.html"; // Redirect to login page if available
+    window.location.href = "index.html"; // redirect to index file
   });
   
