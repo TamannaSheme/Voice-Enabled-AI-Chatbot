0e1b9226cb683c8d86c54ab276471d925416c938
TamannaSheme <syedatsheme03@gmail.com>
2025-05-02 15:34:54 +1000

    Preferences Page Tesing Done

---
+++ tests/preferences_setting.test.js
@@ -0,0 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
+const fs = require("fs");
+const path = require("path");
+
+describe("Preferences Page", () => {
+  let html;
+  let container;
+
+  beforeEach(() => {
+    // 🧠 MOCK ALERT FIRST
+    global.alert = jest.fn();
+
+    html = fs.readFileSync(path.resolve(__dirname, "../preferences_setting.html"), "utf8");
+    document.documentElement.innerHTML = html.toString();
+    require("../js/preferences_setting.js");
+
+    container = document.body;
+  });
+
+  afterEach(() => {
+    jest.resetModules();
+  });
+
+  test("should alert on clicking Save", () => {
+    const saveBtn = container.querySelector('#save-btn');
+    saveBtn.click();
+    expect(global.alert).toHaveBeenCalledWith("Preferences saved successfully!");
+  });
+});
